03c8559bb6a397d8753533795091cccdb2ea309f
squarepusher03 <grambh12@gmail.com>
2021-05-15 01:24:04 -0400

    Screwing around with overriding methods and using the virtual keyword.

--- Chapter 5/Polymorphism/Polymorphism/Polymorphism/Polymorphism.cpp
+++ Chapter 5/Polymorphism/Polymorphism/Polymorphism/Polymorphism.cpp
@@ -4,14 +4,31 @@ class BaseClass {
     const char* holistic_detective = "Dirk Gently";
 
 public:
-    int x() const { return 1; }
+    virtual ~BaseClass() = default;
+    virtual int x() const { return 1; }
+    virtual const char* hello() const = 0;
     const char* member = "gold";
 };
-struct DerivedClass : BaseClass { };
+
+struct DerivedClass : BaseClass { 
+    int y = 0;
+    int x() const override { return 2; }
+    const char* hello() const override { return "hello"; }
+    DerivedClass() { printf("DerivedClass()\n"); }
+    ~DerivedClass() override { printf("~DerivedClass()\n"); }
+
+private:
+    int z = 1;
+};
 
 int main()
 {
+    //BaseClass base; 
     DerivedClass derived;
-    printf("x = %d\n", derived.x());
+    BaseClass* ref = new DerivedClass();
+    //printf("x = %d\n", base.x());
+    printf("y = %d\n", derived.x());
+    printf("z = %d\n", ref->x());
     printf("%s member.\n", derived.member);
+    delete ref;
 }
