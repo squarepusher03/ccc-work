361b90aae3531a539fc646ac421a5aae39da6ece
squarepusher03 <grambh12@gmail.com>
2021-05-18 02:03:13 -0400

    Changed it to Property Injection.

--- Chapter 5/BankLogger/BankLogger/BankLogger/main.cpp
+++ Chapter 5/BankLogger/BankLogger/BankLogger/main.cpp
@@ -19,20 +19,25 @@ struct ConsoleLogger : Logger {
 };
 
 class Bank {
-    Logger& logger;
+    Logger *logger;
 
 public:
-    Bank(Logger& logger) : logger{ logger } { }
+    void setLogger(Logger* logger) {
+        this->logger = logger;
+    }
 
     void makeTransfer(long from, long to, double amount) {
-        logger.logTransfer(from, to, amount);
+        if (logger) logger->logTransfer(from, to, amount);
     }
 };
 
 int main() {
-    ConsoleLogger logger;
-    Bank bank{ logger };
+    ConsoleLogger consoleLogger;
+    FileLogger fileLogger;
+    Bank bank;
+    bank.setLogger(&consoleLogger);
     bank.makeTransfer(1000, 2000, 49.95);
+    bank.setLogger(&fileLogger);
     bank.makeTransfer(2000, 4000, 20.00);
 
     return 0;
