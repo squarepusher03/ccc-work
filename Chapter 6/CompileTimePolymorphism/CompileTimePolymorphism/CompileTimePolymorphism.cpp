fe69a956cfe83ddda699d8d1bde29262c461d393
squarepusher03 <grambh12@gmail.com>
2021-05-26 01:13:14 -0400

    added casting methods

--- Chapter 6/CompileTimePolymorphism/CompileTimePolymorphism/CompileTimePolymorphism.cpp
+++ Chapter 6/CompileTimePolymorphism/CompileTimePolymorphism/CompileTimePolymorphism.cpp
@@ -5,11 +5,22 @@ class MyTemplateClass {
     Z* member;
 
 public:
-    X foo(&Y) { }
+    X foo(Y&) { }
 };
 
 template<typename X, typename Y, typename Z>
 X myTemplateFunction(Y& arg1, const Z* arg2) { }
+
+void carbonThaw(const int& encasedSolo) {
+    auto& hibernationSickSolo = const_cast<int&>(encasedSolo); // casting from const to normal
+    hibernationSickSolo++;
+}
+
+short incrementAsShort(void* target) {
+    auto asShort = static_cast<short*>(target); // what does this even do
+    *asShort += 1;
+    return *asShort;
+}
 
 int main()
 {
