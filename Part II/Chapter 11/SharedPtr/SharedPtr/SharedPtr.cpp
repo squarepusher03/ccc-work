f9d1c027121ed08586a7d71d6da6884645cdebb8
Graham H <grambh12@gmail.com>
2021-09-23 08:16:28 -0400

    Finished up SharedPtr and FileGuard.

---
+++ Part II/Chapter 11/SharedPtr/SharedPtr/SharedPtr.cpp
@@ -0,0 +1,42 @@
+#define CATCH_CONFIG_MAIN
+#include <iostream>
+#include <memory>
+#include "../../../lib/catch.hpp"
+using namespace std;
+
+struct obj {
+	const char* message;
+	static int x;
+
+	obj(const char* m = "") : message{ m } { x++; }
+	~obj() { x--; }
+};
+
+int obj::x{};
+using SharedPtr = shared_ptr<obj>;
+
+TEST_CASE("SharedPtr can be used in copy") {
+	auto a = make_shared<obj>();
+
+	// b shares ownership with a over the obj
+	SECTION("construction") {
+		auto b{ a };
+		REQUIRE(obj::x == 1);
+	}
+
+	// the same here
+	SECTION("assignment") {
+		SharedPtr b;
+		b = a;
+		REQUIRE(obj::x == 1);
+	}
+
+	SECTION("assignment, and original gets discarded") {
+		// b has it's own obj, making the total obj's 2.
+		auto b = make_shared<obj>();
+		REQUIRE(obj::x == 2);
+		// b has it's own obj, making the total obj's 2.
+		b = a;
+		REQUIRE(obj::x == 1);
+	}
+}
--- Part II/Chapter 11/UniquePtr/UniquePtr.cpp
+++ Part II/Chapter 11/UniquePtr/UniquePtr.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <memory>
 #include "../../lib/catch.hpp"
+#pragma warning( disable : 4996 )
 using namespace std;
 
 struct ptr {
@@ -18,30 +19,30 @@ using UniquePtr = std::unique_ptr<ptr>;
 // Unique pointers support every operation that scoped pointers do.
 // They cannot be copied though (exclusive ownership, but transferable)
 
-//TEST_CASE("UniquePtr can be used in move") {
-//	auto a = make_unique<ptr>();
-//	SECTION("construction") {
-//		auto b{ move(a) };
-//		REQUIRE(ptr::x == 1);
-//	}
-//
-//	SECTION("assignment") {
-//		auto b = make_unique<ptr>();
-//		REQUIRE(ptr::x == 2);
-//		b = move(a);
-//		REQUIRE(ptr::x == 1);
-//	}
-//}
-//
-//TEST_CASE("UniquePtr to array supports operator[]") {
-//	unique_ptr<int[]> squares{
-//		new int[5]{ 1, 4, 9, 16, 25 }
-//	};
-//	squares[0] = 1;
-//	REQUIRE(squares[0] == 1);
-//	REQUIRE(squares[1] == 4);
-//	REQUIRE(squares[2] == 9);
-//}
+TEST_CASE("UniquePtr can be used in move") {
+	auto a = make_unique<ptr>();
+	SECTION("construction") {
+		auto b{ move(a) };
+		REQUIRE(ptr::x == 1);
+	}
+
+	SECTION("assignment") {
+		auto b = make_unique<ptr>();
+		REQUIRE(ptr::x == 2);
+		b = move(a);
+		REQUIRE(ptr::x == 1);
+	}
+}
+
+TEST_CASE("UniquePtr to array supports operator[]") {
+	unique_ptr<int[]> squares{
+		new int[5]{ 1, 4, 9, 16, 25 }
+	};
+	squares[0] = 1;
+	REQUIRE(squares[0] == 1);
+	REQUIRE(squares[1] == 4);
+	REQUIRE(squares[2] == 9);
+}
 
 auto my_deleter = [](int* x) {
 	cout << "Deleting unique ptr.\n";
@@ -55,18 +56,18 @@ unique_ptr<int, decltype(my_deleter)> my_up{
 
 // ---------------
 
-using FileGuard = unique_ptr<FILE, int(*)(FILE*)>;
-
-void say_hello(FileGuard file) {
-	fprintf(file.get(), "HELLO DAVE");
-}
-
-int main() {
-	auto file = fopen("HAL9000", "w");
-	if (!file) return errno;
-	FileGuard file_guard{ file, fclose };
-	// File open here
-	say_hello(move(file_guard));
-	// File close here
-	return 0;
-}
+//using FileGuard = unique_ptr<FILE, int(*)(FILE*)>;
+//
+//void say_hello(FileGuard file) {
+//	fprintf(file.get(), "HELLO DAVE");
+//}
+//
+//int main() {
+//	auto file = fopen("HAL9000", "w"); 
+//	if (!file) return errno;
+//	FileGuard file_guard{ file, fclose };
+//	// File open here
+//	say_hello(move(file_guard));
+//	// File close here
+//	return 0;
+//}
