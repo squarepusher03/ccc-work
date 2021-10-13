9cbf5ed5a999716aac808847c36f9fcfc71887cb
Graham H <grambh12@gmail.com>
2021-10-13 08:18:05 -0400

    More work on strings (why is there so much content)

--- Part II/Chapter 15/StringsAgain/StringsAgain/StringsAgain.cpp
+++ Part II/Chapter 15/StringsAgain/StringsAgain/StringsAgain.cpp
@@ -1,6 +1,9 @@
 #define CATCH_CONFIG_MAIN
 #include <catch.hpp>
+#include <boost/algorithm/string/predicate.hpp>
+#include <string_view>
 #include <string>
+using namespace boost::algorithm;
 using namespace std::literals::string_literals;
 
 TEST_CASE("std::string supports constructing") {
@@ -231,16 +234,110 @@ TEST_CASE("STL string conversion function") {
         REQUIRE("8975309"s == std::to_string(8975309));
     }
 
-    SECTION("to_wstring") {
+    /* SECTION("to_wstring") {
         // fails on some systems due to double type precision inaccuracies
-        REQUIRE(L"109951.1627776"s == std::to_wstring(109951.1627776)); 
-    }
+        REQUIRE(L"109951.1627776"s == std::to_wstring(109951.1627776));
+    } */
 
     SECTION("stoi") {
         REQUIRE(std::stoi("8675309"s) == 8675309);
     }
 
-    SECTION("stoul with all valid characters") {
-
+    SECTION("stoi") {
+        REQUIRE_THROWS_AS(std::stoi("109951162776"s), std::out_of_range);
     }
+
+    SECTION("stoul with all valid characters") {
+        size_t last_character{};
+        // what to parse, the length of the string, the hexadecimal base
+        const auto result = std::stoul("0xD3C34C3D"s, &last_character, 16);
+    }
+
+    SECTION("stoul") {
+        size_t last_character{};
+        const auto result = std::stoul("42six"s, &last_character);
+        REQUIRE(result == 42);
+        REQUIRE(last_character == 2);
+    }
+
+    SECTION("stod") {
+        REQUIRE(std::stod("2.7182818"s) == Approx(2.7182818));
+    }
+}
+
+TEST_CASE("std::string_view supports") {
+    SECTION("default construction") {
+        std::string_view view;
+        REQUIRE(view.data() == nullptr);
+        REQUIRE(view.size() == 0);
+        REQUIRE(view.empty());
+    }
+
+    SECTION("construction from string") {
+        std::string word("sacrosanct");
+        std::string_view view(word);
+        REQUIRE(view == "sacrosanct");
+    }
+
+    SECTION("construction from C-string") {
+        auto word = "viewership";
+        std::string_view view(word);
+        REQUIRE(view == "viewership");
+    }
+
+    SECTION("construction from C-string and length") {
+        auto word = "viewership";
+        std::string_view view(word, 4);
+        REQUIRE(view == "view");
+    }
+}
+
+TEST_CASE("string_view is modifiable with") {
+    std::string_view view("previewing");
+    
+    SECTION("remove_prefix") {
+        view.remove_prefix(3);
+        REQUIRE(view == "viewing");
+    }
+
+    SECTION("remove_suffix") {
+        view.remove_suffix(3);
+        REQUIRE(view == "preview");
+    }
+}
+
+size_t count_v(std::string_view my_view) {
+    size_t result{};
+    for (auto letter : my_view) {
+        if (letter == 'v' || letter == 'V') result++;
+    }
+
+    return result;
+}
+
+TEST_CASE("count_v") {
+    SECTION("counts vs in passed string") {
+        REQUIRE(count_v("very valorant valkrie") == 3);
+    }
+
+    SECTION("counts both upper and lower case") {
+        REQUIRE(count_v("Valorie Vaughn was a very valorant valkrie") == 5);
+    }
+}
+
+TEST_CASE("boost::algorithm") {
+    std::string word("cymotrichous");
+    
+    SECTION("starts_with tests a string's beginning") {
+        REQUIRE(starts_with(word, "cymo"s));
+    }
+
+    SECTION("istarts_with is case insensitive") {
+        REQUIRE(istarts_with(word, "cYmO"s));
+    }
+}
+
+TEST_CASE("boost::algorithm::all evaluates a predicate for all elements") {
+    std::string word("juju");
+    REQUIRE(all(word, [](auto c) { return c == 'j' || c == 'u'; }));
 }
